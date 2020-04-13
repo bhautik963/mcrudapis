824ebc43f5b3dc30eedf79d8cab22ac01b9c8552
bhautik123 <63495794+bhautik123@users.noreply.github.com>
2020-04-13 11:06:05 +0530

    Update appRoutes.js

--- Routes/appRoutes.js
+++ Routes/appRoutes.js
@@ -3,78 +3,88 @@ var ObjectId = require('mongodb').ObjectID;
 var mongojs = require('mongojs');
 var router = express.Router();
 
-var db = mongojs("mongodb+srv://studentdb:4CsQdyR5ALasHzLb@mycluster-jtfqi.mongodb.net/student?retryWrites=true&w=majority"); //db connection
+var db = mongojs("mongodb+srv://bhautik:1234567890@angularcurd-hbmz8.mongodb.net/bhautik?retryWrites=true&w=majority"); //db connection
 
 //reading data
-router.get('/', (req, res) => {
-    db.student.find({}, (err, msg) => {
-        if (!err) {
-            res.status(200).json({
-                message: msg
-            })
-        } else {
-            res.status(500).json({
-                message: err
-            });
-        }
-    });
-})
-
-
-//adding data
-
-router.post('/addstud', (req, res) => {
-
-    db.student.save({
-        name: req.body.name,
-        pswd: req.body.pswd
-    }, (err, msg) => {
-        if (!err) {
-            res.status(200).json({
-                message: msg
-            });
-        } else {
-            res.status(500).json({
-                message: err
-            });
-        }
-    });
-
+router.get('/', function (req, res) {
+    res.send("hello from server 1234 !!!!");
 });
 
-
-// //updating records
-router.put('/updatestud',(req,res)=>{
-    
-    db.student.update({_id:ObjectId(req.body._id)},{$set:{name:req.body.name,pswd:req.body.pswd}},(err,msg)=>{
-        if (!err) {
-            res.status(200).json({
-                message: msg
-            });
-        } else {
-            res.status(500).json({
-                message: err
-            });
-        }
+router.post('/addstud', function (req, res) {
+    console.log(req.body);
+    db.bhautik.save({
+        Enrollment_No: req.body.Enrollmentno,
+        Name: req.body.Name,
+        Age:req.body.age,
+        Birth_Date:req.body.Bod,
+        Department:req.body.dept,
+        Branch:req.body.branch,
+        Result:req.body.result,
+        Fees:req.body.fees
     })
-})
-
-
-
-// //deleting records
-router.delete('/delete/:id', (req, res, next) => {
-    console.log("deelete");
-    var t = req.params.id.toString()
-    console.log(t);
-    
-    db.student.remove({ _id: ObjectId(t) }, (err, msg) => {
-        if (err) {
-            res.status(500).json(err);
-        } else {
-            res.status(200).json({ msg: msg });
-        }
+    res.status(200).send({
+        "message": "data received"
     });
 });
+
+// Get single employee
+router.get('/read/:id',(req, res) => {
+    db.bhautik.findOne({_id: mongoose.ObjectId(req.params.id)}, (error, data) => {
+      if (error) {
+        return next(error)
+      } else {
+        res.json(data)
+		//console.log(data);
+
+        
+      }
+    })
+  })
+
+   // Update employee
+router.put('/update/:id',(req, res, next) => {
+    db.bhautik.update({_id: mongoose.ObjectId(req.params.id)}, {
+      $set: req.body
+    }, (error, data) => {
+      if (error) {
+        return next(error);
+        console.log(error)
+      } else {
+        
+        console.log('Data updated successfully')
+      }
+    })
+  })
+
+
+// get user
+
+router.get("/getuser",function(req,res){  
+    db.bhautik.find(function(err,data){  
+              if(err){  
+                  res.send(err);  
+              }  
+              else{               
+                  res.json(data);
+		      consol.log(data);
+                  }  
+          });  
+  });
+
+
+  //delete user
+  router.delete('/delete/:id',(req, res, next) => {
+    db.bhautik.remove({_id: mongoose.ObjectId(req.params.id)}, (error, data) => {
+      if (error) {
+        return next(error);
+      } else {
+        res.status(200).json({
+          msg: "dele"
+        })
+      }
+    })
+  })
+
 
 
 
